27deed1f0b917747e31f7b522eda58a6e139639d
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:15:54 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/Fatorial.java
@@ -0,0 +1,14 @@
+package com.trybe.acc.java;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+	    //Seu código aqui
+		
+			
+			// Imprime a saída no console. OBS: descomente a próxima linha
+			//System.out.println("O fatorial de 18 é igual a " + fatorial18);
+
+	}
+
+}
